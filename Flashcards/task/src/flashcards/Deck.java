559f03e3a843f2565e7db4878ea3583b7abae330
BartlomiejBak <bakbartlomiej88>
2020-04-01 21:25:56 +0200

    Implemented stage #5 Menu, please. Now there is user menu, new methods and mechanics.

--- Flashcards/task/src/flashcards/Deck.java
+++ Flashcards/task/src/flashcards/Deck.java
@@ -1,53 +1,107 @@
 package flashcards;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Deck {
-    protected int size;
     protected LinkedHashMap<String, String> deck;
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<String> keys = new ArrayList<>();
 
-    public Deck(int size) {
-        this.size = size;
+    public Deck() {
         this.deck = new LinkedHashMap<>();
     }
 
-    public void fillDeck() {
-        Scanner in = new Scanner(System.in);
-        String key;
-        String def;
-
-        for (int i = 0; i < this.size; i++) {
-            System.out.printf("The card #%d:%n", i + 1);
-            key = in.nextLine();
-            while (deck.containsValue(key)) {
-                System.out.println("The card \"" + key + "\" already exists.");
-                key = in.nextLine();
-            }
-            System.out.printf("The definition of the card #%d:%n", i + 1);
-            def = in.nextLine();
-            while (deck.containsKey(def)) {
+    public void add() {
+        System.out.println("The card:");
+        String key = scanner.nextLine();
+        if (this.deck.containsValue(key)) {
+            System.out.println("The card \"" + key + "\" already exists.");
+        } else {
+            System.out.println("The definition of the card:");
+            String def = scanner.nextLine();
+            if (this.deck.containsKey(def)) {
                 System.out.println("The definition \"" + def + "\" already exists.");
-                def = in.nextLine();
+            } else {
+                this.deck.put(key, def);
+                this.keys.add(key);
+                System.out.println("The pair (\"" + key + "\":\"" + def + "\") has been added.");
             }
-            //this solution simplifies learn() method
-            deck.put(def, key);
         }
     }
 
-    public void learn () {
-        Scanner in = new Scanner(System.in);
-        for (String s : deck.keySet()) {
-            System.out.println("Print the definition of \"" + deck.get(s) + "\":");
-            String def = in.nextLine();
-            if (def.equals(s)) {
+    public void remove() {
+        System.out.println("The card:");
+        String key = scanner.nextLine();
+        if (this.deck.containsKey(key)) {
+            this.deck.remove(key);
+            this.keys.remove(key);
+            System.out.println("The card has been removed.");
+        } else {
+            System.out.println("Can't remove \"" + key + "\": there is no such card.");
+        }
+    }
+
+    public void export() throws IOException {
+        System.out.println("File name:");
+        File file = new File(scanner.nextLine());
+        PrintWriter writer = new PrintWriter(file);
+        for (String s : this.deck.keySet()) {
+            writer.println(s);
+            writer.println(this.deck.get(s));
+        }
+        writer.close();
+        System.out.println(this.deck.size() + " cards have been saved.");
+    }
+
+    public void importDeck() throws FileNotFoundException {
+        System.out.println("File name:");
+        File file = new File(scanner.nextLine());
+        if (file.exists()) {
+            Scanner reader = new Scanner(file);
+            this.keys.clear();
+            int newEntries = 0;
+            while (reader.hasNext()) {
+                String key = reader.nextLine();
+                this.deck.put(key, reader.nextLine());
+                this.keys.add(key);
+                newEntries++;
+            }
+            System.out.println(newEntries + " cards have been loaded.");
+        } else {
+            System.out.println("File not found.");
+        }
+    }
+
+    public void ask() {
+        System.out.println("How many times to ask?");
+        int n = Integer.parseInt(scanner.nextLine());
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            int card = random.nextInt(this.deck.size());
+            String key = this.keys.get(card);
+            System.out.println("Print the definition of \"" + key + "\":");
+            String def = scanner.nextLine();
+            if (def.equals(this.deck.get(key))) {
                 System.out.println("Correct answer.");
-            } else if (deck.containsKey(def)) {
-                System.out.println("Wrong answer. The correct one is \"" + s +
-                        "\", you've just written the definition of \"" + deck.get(def) + "\"");
+            } else if (this.deck.containsValue(def)) {
+                System.out.println("Wrong answer. The correct one is \"" + this.deck.get(key) + "\", "
+                        + "you've just written the definition of \"" + this.getKey(def) + "\".");
             } else {
-                System.out.println("Wrong answer. The correct one is \"" + s + "\"");
+                System.out.println("Wrong answer. The correct one is \"" + this.deck.get(key) + "\".");
             }
         }
+    }
+
+    public String getKey(String key) {
+        for (String s : this.deck.keySet()) {
+            if (key.equals(this.deck.get(s))) {
+                key = s;
+            }
+        }
+        return key;
     }
 }
--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,14 +1,47 @@
 package flashcards;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
-        System.out.println("Input the number of cards:");
-        int num = Integer.parseInt(in.nextLine());
-        Deck deck = new Deck(num);
-        deck.fillDeck();
-        deck.learn();
+        Deck deck = new Deck();
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Input the action (add, remove, import, export, ask, exit):");
+            String action = in.next();
+            switch (action) {
+                case "add":
+                    deck.add();
+                    System.out.println();
+                    break;
+                case "remove":
+                    deck.remove();
+                    System.out.println();
+                    break;
+                case "import":
+                    deck.importDeck();
+                    System.out.println();
+                    break;
+                case "export":
+                    deck.export();
+                    System.out.println();
+                    break;
+                case "ask":
+                    deck.ask();
+                    System.out.println();
+                    break;
+                case "exit":
+                    System.out.println("Bye bye!");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Wrong command.");
+                    for (String key : deck.keys) {
+                        System.out.println(key);
+                    }
+            }
+        }
     }
 }
